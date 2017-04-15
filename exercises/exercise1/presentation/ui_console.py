a9d57a5dd5c1deed47cc70120651edb8afa798ac
Ezequiel Leonardo Castaño <elcg@gmx.com>
2017-04-14 23:10:26 -0300

    Make module instead of class

--- exercises/exercise1/presentation/ui_console.py
+++ exercises/exercise1/presentation/ui_console.py
@@ -3,61 +3,16 @@
 class Console():
     """Console Presentation View"""
 
-    @staticmethod
-    def clear_screen():
-        """Clears the terminal screen."""
-        if system_name().lower() == "windows":
-            command = "cls"
-        else:
-            command = "clear"
-
-        system_call(command)
-
     @classmethod
-    def main(cls):
-        menu = cls.__create_menu()
+    def main():
+        menu = menu_build()
         menu.execute()
 
-    @staticmethod
-    def __create_menu():
-        return Menu_Builder.build()
-
     @classmethod
-    def execute(cls):
+    def execute():
         """Execute the main algorithm and show the report"""
         Algorithm()
-        cls.full_report()
-
-    @classmethod
-    def full_report(cls):
-        """Show all the reports"""
-        cls.generations_report()
-        cls.solution_report()
-
-    @classmethod
-    def generations_report(cls):
-        """Show the generations report"""
-        data = Settings.load_results()
-        data_pd = cls.__get_data_frame(data)
-
-        gens = [i for i, _ in enumerate(data)]
-
-        data_frame = pd.DataFrame.from_items(data_pd, orient='index', columns=gens)
-        data_frame_int = data_frame.astype(int)
-
-        print("Reporte detallado por generacion:\n")
-        print(data_frame_int)
-        print()
-
-    @classmethod
-    def solution_report(cls):
-        """Show the final solution infered from the generations"""
-        data = Settings.load_results()
-        last_population = data[-1]
-        solution = last_population.maximum
-        solution_string = "La solución final es: {}".format(solution)
-        cls.__border(solution_string)
-
+        report.full_report()
 
     @staticmethod
     def __border(printable_string):
@@ -66,39 +21,6 @@ class Console():
         print(printable_string)
         print(border)
 
-
-    @classmethod
-    def __get_data_frame(cls, data):
-        datas = cls.__get_array_data(data)
-        labels = ("Máximo", "Mínimo", "Promedio", "Mínimos cuadrados", "Rango")
-        return [(i, j) for i, j in zip(labels, datas)]
-
-    @staticmethod
-    def __get_array_data(data):
-        maximums = [i.maximum for i in data]
-        minimums = [i.minimum for i in data]
-        averages = [i.average for i in data]
-        leasts = [i.least for i in data]
-        ranges = [i.range for i in data]
-        return (maximums, minimums, averages, leasts, ranges)
-
-    @staticmethod
-    def __header_legacy():
-        """Show basic Configurations."""
-        settings = Settings.load_all_settings()
-        population_size = settings["population_size"]
-        cross_over_prob = settings["cross_over_prob"]
-        mutation_prob = settings["mutation_prob"]
-        generations = settings["generations"]
-        report = settings["report"]
-        print("##########################################")
-        print("Población inicial: {}".format(population_size))
-        print("Probabilidad de CrossOver: {}".format(cross_over_prob))
-        print("Probabilidad de mutacion: {}".format(mutation_prob))
-        print("Numero de generaciones maximas: {}".format(generations))
-        print("Mostrar reporte: {}".format(report))
-        print("##########################################")
-        print()
 
     @staticmethod
     def show_settings():
@@ -112,7 +34,6 @@ class Console():
         print()
         print("La nueva configuración es: ")
         cls.show_settings()
-
 
     @classmethod
     def set_cross_over_prob(cls):
@@ -161,75 +82,20 @@ class Console():
         boolean_value = value == "1"
         Settings.set_report(boolean_value)
         cls.__updated_settings()
-    
-    @staticmethod
-    def extract_data():
-        data = Settings.load_results()
-        maximums = [i.maximum for i in data]
-        minimums = [i.minimum for i in data]
-        averages = [i.average for i in data]
-        leasts = [i.least for i in data]
-        ranges = [i.range for i in data]
-
-        datas = [maximums, minimums, averages, leasts, ranges]
-        labels = ["Maximo", "Minimo", "Promedio", "Cuadrados", "Rango"]
-        return datas, labels
-
-    @staticmethod
-    def graphics(datas, labels):
-        """Plot several graph with its labels."""
-        plt.figure(figsize=(17, 9))
-        ax1 = plt.subplot2grid((1, 1), (0, 0))
-        ax1.grid(True)
-
-        plt.xlabel('Generaciones')
-        plt.title('Algoritmo Genético')
-
-
-        for data, label in zip(datas, labels):
-            x_data = [i for i, _ in enumerate(data)]
-            ax1.set_xticks(x_data)
-            y_data = data
-            #print(data)
-            maximum = int(max(data))
-            sticks = (i for i in range(0, maximum+1, 2))
-            unique_sticks = set(sticks)
-            yticsk = sorted(unique_sticks)
-            #ax1.set_yticks(yticsk)
-            ax1.plot(x_data, y_data, label=label)
-        plt.legend()
-        plt.show()
-    
-    @classmethod
-    def graphic_mma(cls):
-        datas, labels = cls.extract_data()
-        cls.graphics(datas[:3],labels[:3])
-
-    @classmethod
-    def graphic_ls(cls):
-        datas, labels = cls.extract_data()
-        cls.graphics(datas[3:4],labels[3:4])
-
-    @classmethod
-    def graphic_r(cls):
-        datas, labels = cls.extract_data()
-        cls.graphics(datas[4:5],labels[4:5])
 
 
 if __name__ == "__main__":
     import pandas as pd
-    from platform import system as system_name
-    from os import system as system_call
     from exercise1.logic.settings import Settings
     from exercise1.logic.algorithm import Algorithm
-    from exercise1.presentation.ui_console_menu_builder import Menu_Builder
-
+    from exercise1.presentation.ui_console_menu_builder import build as menu_build
+    import exercise1.logic.report as report
     Console.main()
+
 if __name__ != "__main__":
     import pandas as pd
-    import matplotlib.pyplot as plt
-    from os import system as system_call
-    from platform import system as system_name
     from exercise1.logic.settings import Settings
     from exercise1.logic.algorithm import Algorithm
-    from exercise1.presentation.ui_console_menu_builder import Menu_Builder
+    from exercise1.presentation.ui_console_menu_builder import build as menu_build
+    import exercise1.logic.report as report
+
--- exercises/exercise1/presentation/ui_console_menu_builder.py
+++ exercises/exercise1/presentation/ui_console_menu_builder.py
@@ -1,94 +1,85 @@
 """Builds and returns a Menu"""
 
-class Menu_Builder():
-    """Builds and returns a Menu"""
+def build():
+    """Create Menu"""
+    report_sub_menu = __generate_report_menu()
+    settings_sub_menu = __generate_settings_menu()
+    graphic_sub_menu = __generate_graphic_menu()
+    submenus = [
+        report_sub_menu,
+        graphic_sub_menu,
+        settings_sub_menu
+    ]
+    main_menu = __generate_main_menu(submenus)
 
-    @classmethod
-    def build(cls):
-        """Create Menu"""
-        report_sub_menu = cls.__generate_report_menu()
-        settings_sub_menu = cls.__generate_settings_menu()
-        graphic_sub_menu = cls.__generate_graphic_menu()
-        submenus = [
-            report_sub_menu,
-            graphic_sub_menu,
-            settings_sub_menu
-        ]
-        main_menu = cls.__generate_main_menu(submenus)
+    return main_menu
 
-        return main_menu
+def __generate_main_menu(submenus):
+    """Generate the main manu"""
+    menu = Menu("Principal")
 
-    @staticmethod
-    def __generate_main_menu( submenus):
-        """Generate the main manu"""
-        menu = Menu("Principal")
+    items = [Item("Ejecutar", Console.execute),
+            ]
 
-        items = [Item("Ejecutar", Console.execute),
-                ]
+    for submenu in submenus:
+        item = Item(submenu.name, submenu.execute)
+        items.append(item)
 
-        for submenu in submenus:
-            item = Item(submenu.name, submenu.execute)
-            items.append(item)
+    for item in items:
+        menu.add_item(item)
 
-        for item in items:
-            menu.add_item(item)
+    return menu
 
-        return menu
+def __generate_report_menu():
+    report_sub_menu = Menu("Reporte")
+    report_sub_menu_items = [
+        Item("Reporte Completo", report.full_report),
+        Item("Reporte de Generaciones", report.generations_report)
+    ]
+    report_sub_menu = _generate_sub_menu(
+        report_sub_menu,
+        report_sub_menu_items
+        )
+    return report_sub_menu
 
+def __generate_settings_menu():
+    settings_sub_menu = Menu("Configuracion")
+    settings_sub_menu_items = [
+        Item("Mostrar configuracion actual", Console.show_settings),
+        Item("Cambiar probabilidad de Cross Over", Console.set_cross_over_prob),
+        Item("Cambiar cantidad de bits de los individuos", Console.set_individual_bits),
+        Item("Cambiar cantidad maxima de generaciones", Console.set_generations),
+        Item("Cambiar probabilidad de mutación", Console.set_mutation_prob),
+        Item("Cambiar Tamaño inicial de la población", Console.set_population_size),
+        Item("Cambiar mostrar reporte (en desuso)", Console.set_report)
+    ]
+    settings_sub_menu = _generate_sub_menu(
+        settings_sub_menu,
+        settings_sub_menu_items
+        )
+    return settings_sub_menu
 
-    @classmethod
-    def __generate_report_menu(cls):
-        report_sub_menu = Menu("Reporte")
-        report_sub_menu_items = [
-            Item("Reporte Completo", Console.full_report),
-            Item("Reporte de Generaciones", Console.generations_report)
-        ]
-        report_sub_menu = cls.__generate_sub_menu(
-            report_sub_menu,
-            report_sub_menu_items
-            )
-        return report_sub_menu
+def __generate_graphic_menu():
+    graphic_sub_menu = Menu("Graficos")
+    graphic_sub_menu_items = [
+        Item("Graficar Maximos, minimos y promedios", graphics.graphic_mma),
+        Item("Graficar mínimos cuadrados", graphics.graphic_ls),
+        Item("Graficar Rango", graphics.graphic_r),
+    ]
+    graphic_sub_menu = _generate_sub_menu(
+        graphic_sub_menu,
+        graphic_sub_menu_items
+        )
+    return graphic_sub_menu
 
-    @classmethod
-    def __generate_settings_menu(cls):
-        settings_sub_menu = Menu("Configuracion")
-        settings_sub_menu_items = [
-            Item("Mostrar configuracion actual", Console.show_settings),
-            Item("Cambiar probabilidad de Cross Over", Console.set_cross_over_prob),
-            Item("Cambiar cantidad de bits de los individuos", Console.set_individual_bits),
-            Item("Cambiar cantidad maxima de generaciones", Console.set_generations),
-            Item("Cambiar probabilidad de mutación", Console.set_mutation_prob),
-            Item("Cambiar Tamaño inicial de la población", Console.set_population_size),
-            Item("Cambiar mostrar reporte (en desuso)", Console.set_report)
-        ]
-        settings_sub_menu = cls.__generate_sub_menu(
-            settings_sub_menu,
-            settings_sub_menu_items
-            )
-        return settings_sub_menu
-
-    @classmethod
-    def __generate_graphic_menu(cls):
-        graphic_sub_menu = Menu("Graficos")
-        graphic_sub_menu_items = [
-            Item("Graficar Maximos, minimos y promedios", Console.graphic_mma),
-            Item("Graficar mínimos cuadrados", Console.graphic_ls),
-            Item("Graficar Rango", Console.graphic_r),
-        ]
-        graphic_sub_menu = cls.__generate_sub_menu(
-            graphic_sub_menu,
-            graphic_sub_menu_items
-            )
-        return graphic_sub_menu
-
-    @staticmethod
-    def __generate_sub_menu(sub_menu, items):
-        for item in items:
-            sub_menu.add_item(item)
-        return sub_menu
-
+def _generate_sub_menu(sub_menu, items):
+    for item in items:
+        sub_menu.add_item(item)
+    return sub_menu
 
 if __name__ != "__main__":
     from exercise1.presentation.ui_console_menu import Menu
     from exercise1.presentation.ui_console_item import Item
     from exercise1.presentation.ui_console import Console 
+    import exercise1.presentation.graphics as graphics
+    import exercise1.logic.report as report
