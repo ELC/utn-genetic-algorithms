89a708cf003547a83a62a4905b09d73b54d590e1
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-10-17 08:50:34 -0300

    ✨ Add Auto-Centering

--- exercises/exercise4/fractal.py
+++ exercises/exercise4/fractal.py
@@ -1,6 +1,7 @@
 import turtle
 import colorsys
 import os
+from math import cos, sin, radians
 
 def create_l_system(iters, axiom, rules):
     start_string = axiom
@@ -66,23 +67,115 @@ def draw_background(t):
     t.speed(turtlespeed)
 
 
-def main(iterations, axiom, rules, angle, length, size=2,
-        y_offset=None, x_offset=None, offset_angle=0, 
-        color=False, filename=None, width=None, height=None, margin=35, aspect_ratio=1):
-
-    if width is None and height:
-        width = aspect_ratio / height
-
-    if height is None and width:
-        height = aspect_ratio * width
-
-    if x_offset is None:
-        x_offset = -(width / 2 - margin)
+def calc_length_height(instructions, angle, correction_angle):
+    current_angle = correction_angle
+    x_offset = 0
+    y_offset = 0
+    min_x = 0
+    min_y = 0
+    max_x = 0
+    max_y = 0
+    for inst in instructions:
+        if inst == "F":
+            x_offset += cos(radians(current_angle))
+            y_offset += sin(radians(current_angle))
+        elif inst == "B":
+            x_offset -= cos(radians(current_angle))
+            y_offset -= sin(radians(current_angle))
+        elif inst == "+":
+            current_angle -= angle
+        elif inst == "-":
+            current_angle += angle
+        max_x = max(max_x, x_offset)
+        min_x = min(min_x, x_offset)
+        max_y = max(max_y, y_offset)
+        min_y = min(min_y, y_offset)
     
-    if y_offset is None:
-        y_offset = -(height / 2 - margin)
+    width = abs(max_x) + abs(min_x)
+    height = abs(max_y) + abs(min_y)
+
+
+    return width, height, abs(min_x), abs(min_y)
+
+def main(iterations, axiom, rules, angle, length=None, size=None, correction_angle=0,
+        y_offset=None, x_offset=None, offset_angle=None, inverted=False, flip_h=False, flip_v=False,
+        color=False, filename=None, width=None, height=None, margin=None, aspect_ratio=None):
 
     inst = create_l_system(iterations, axiom, rules)
+
+    width_, height_, min_x, min_y = calc_length_height(inst, angle, correction_angle)    
+
+    if aspect_ratio is None:
+        if 0 in [width_, height_]:
+            aspect_ratio = 1
+        else:
+            aspect_ratio = width_ / height_
+
+    if width is None and height:
+        width = height / aspect_ratio
+
+    if height is None and width:
+        height = width / aspect_ratio
+    
+    if margin is None:
+        margin = 35
+
+    if offset_angle is None:
+        offset_angle = -90
+
+    if length is None:
+        if width_ > height_:
+            length = (width - 2 * margin) / width_
+        else:
+            length = (height - 2 * margin) / height_
+    
+    if width_ * length > width:
+        length = (width - 2 * margin) / width_
+    elif height_ * length > height:
+        length = (height - 2 * margin) / height_
+    
+    if x_offset is None:
+        if width_ >= height_  and (width - width_) <= width_ - 2 * margin :
+            x_offset = -(width / 2 - margin) + min_x * length
+        else:
+            x_offset = -(width / 2) + (width - width_ * length) / 2 + min_x * length
+    
+    if y_offset is None:
+        if height_ >= width_ and (height - height_) <= height_ - 2 * margin :
+            y_offset = -(height / 2 - margin) + min_y * length
+        else:
+            y_offset = -(height / 2) + (height - height_ * length) / 2 + min_y * length
+
+
+    if inverted:
+        inst = inst.replace('+', '$')
+        inst = inst.replace('-', '+')
+        inst = inst.replace('$', '-')
+        inst = inst.replace('F', '$')
+        inst = inst.replace('B', 'F')
+        inst = inst.replace('$', 'B')
+    
+    if flip_h:
+        inst = inst.replace('F', '$')
+        inst = inst.replace('B', 'F')
+        inst = inst.replace('$', 'B')
+        y_offset = -y_offset
+
+    if flip_v:
+        inst = inst.replace('+', '$')
+        inst = inst.replace('-', '+')
+        inst = inst.replace('$', '-')
+        y_offset = -y_offset
+
+    print(f"Width: {width_} - Height:{height_ }")
+
+    print(length)
+
+    if size is None:
+        if length < 2:
+            size = 1
+        else:
+            size = 2
 
     t = turtle.Turtle()
     wn = turtle.Screen()
@@ -124,91 +217,60 @@ def main(iterations, axiom, rules, angle, length, size=2,
 title = "32-segment curve"
 axiom = "F+F+F+F"
 rules = {"F":"-F+F-F-F+F+FF-F+F+FF+F-F-FF+FF-FF+F+F-FF-F-F+FF-F-F+F+F-F+"}
-iterations = 2
+iterations = 1
 angle = 90
-width = 450
-height = 450
-margin = 35
-length = (width - 2 * margin ) / 135
-x_offset = -32*length
-y_offset = 190-36*length
-angle_offset = -90
+width = 500
 
-# 1 16 135 
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
 # box fractal
 title = "box fractal"
 axiom = "F-F-F-F"
 rules = {"F":"F-F+F+F-F"}
-iterations = 6
+iterations = 2
 angle = 90
 width = 450
-margin = 35
-length = (width - 2 * margin) / 3 ** (iterations)
-angle_offset = -90
 
-# 1 3 9 
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
 # Dragon curve
 title = "Dragon curve"
 axiom = "FX"
 rules = {"X":"X+YF+", "Y":"-FX-Y"}
-iterations = 12
-angle = 90
-width = 450
-height = 450
-margin = 35
-length = (width - 2 * margin) / (2 * 74)
-x_offset = 0
-y_offset = 0
-angle_offset = -90
-
-# 2 2 4 4 8 10 18 20 36 42 74 84 148
-
-# Dragon curve
-title = "Dragon curve v 2"
-axiom = "FX"
-rules = {"X":"X+YF+", "Y":"-FX-Y"}
 iterations = 0
 angle = 90
-width = 450
-height = 450
-margin = 35
-length = (width - 2 * margin) / (95)
-x_offset =  width / 2 - margin - 21 * length#
-y_offset = (width / 2) - ( (width - 63 * length) / 2 + 21 * length)
-angle_offset = -90
+width = 500
 
-# 2 2 4 4 5 7 15 15 24 31 47 62 95
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
-
-main(iterations, axiom, rules, angle, size=2, x_offset=x_offset, y_offset=y_offset,
-    length=length, offset_angle=angle_offset,  
-    color=True, width=width, height=height,  filename=title.replace(" ","-"))
+# 2 2 4 4 8 10 18 20 36 42 74 84 148
 
 # Hilbert curve
 title = "Hilbert curve"
 axiom = "L"
 rules = {"L":"+RF-LFL-FR+", "R":"-LF+RFR+FL-"}
-iterations = 9
+iterations = 3
 angle = 90
 width = 450
-height = 450
-x_offset = -190
-y_offset = -190 
-angle_offset = 0
+y_offset = -190
+angle_offset = 90
+
+# main(iterations, axiom, rules, angle, color=True, width=width, inverted=True,
+#         offset_angle=angle_offset, y_offset=y_offset) # ,  filename=title.replace(" ","-"))
 
 # Hilbert curve II
 title = "Hilbert curve II"
 axiom = "X"
 rules = {"X":"XFYFX+F+YFXFY-F-XFYFX", "Y":"YFXFY-F-XFYFX+F+YFXFY"}
-iterations = 6
+iterations = 4
 angle = 90
 width = 450
-height = 450
-x_offset = -190
 y_offset = -190
 angle_offset = 0
+
+# main(iterations, axiom, rules, angle, color=True, width=width, 
+#           offset_angle=angle_offset, y_offset=y_offset) # ,  filename=title.replace(" ","-"))
+
 
 # 2 8 26 80 242 728
 
@@ -216,13 +278,11 @@ angle_offset = 0
 title = "Koch snowflake"
 axiom = "F--F--F"
 rules = {"F":"F+F--F+F"}
-iterations = 6
+iterations = 0
 angle = 60
 width = 450
-height = 500
-x_offset = -190
-y_offset = -235+380/3
-angle_offset = -90
+
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
 # 1 3 9 27 81 243 729
 
@@ -230,13 +290,11 @@ angle_offset = -90
 title = "Peano curve"
 axiom = "F"
 rules = {"F":"F+F-F-F-F+F+F+F-F"}
-iterations = 6
+iterations = 2
 angle = 90
 width = 450
-height = 450
-x_offset = -190
-y_offset = 0
-angle_offset = -90
+
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
 # 1 3 9 27 81 243 729
 
@@ -244,13 +302,11 @@ angle_offset = -90
 title = "Peano-Gosper curve"
 axiom = "FX"
 rules = {"X":"X+YF++YF-FX--FXFX-YF+", "Y":"-FX+YFYF++YF+FX--FX-Y"}
-iterations = 5
+iterations = 4
 angle = 60
 width = 450
-height = 450
-x_offset = -30
-y_offset = 190 
-angle_offset = -90+30+15
+
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
 # ? 2.5 7 20 55 150
 
@@ -258,7 +314,7 @@ angle_offset = -90+30+15
 title = "quadratic Koch island"
 axiom = "F+F+F+F"
 rules = {"F":"F-F+F+FFF-F-F+F"}
-iterations = 3
+iterations = 2
 angle = 90
 width = 450
 height = 450
@@ -266,6 +322,7 @@ x_offset = -65
 y_offset = 145
 angle_offset = -90+20
 
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
 # 1 7 31 127
 
@@ -273,7 +330,7 @@ angle_offset = -90+20
 title = "Sierpiński arrowhead"
 axiom = "YF"
 rules = {"X":"YF+XF+Y", "Y":"XF-YF-X"}
-iterations = 2
+iterations = 1
 angle = 60
 width = 450
 aspect_ratio = 9 / 8
@@ -282,28 +339,33 @@ length = (width - 2 * margin) / 2**(iterations+1)
 y_offset = -150
 x_offset = 190
 angle_offset = 90
+correction_angle = 0 if iterations % 2 == 0 else -60
+offset_angle = -90 if iterations % 2 == 0 else -30
+flip_v = iterations % 2 == 0
+
+# main(iterations, axiom, rules, angle, flip_v=True, 
+#         correction_angle=correction_angle, offset_angle=offset_angle, color=True, width=width, height=width) # ,  filename=title.replace(" ","-"))
 
 # Sierpiński curve
 title = "Sierpiński curve"
 axiom = "F+XF+F+XF"
 rules = {"X":"XF-F+F-XF+F+XF-F+F-X"}
-iterations = 8
+iterations = 4
 angle = 90
 width = 450
-margin = 35
-length = (width - 2 * margin) / (2 ** (iterations+2) - 3)
-x_offset = -length*0.5
-y_offset = (width - 2 * margin) /2
-angle_offset = -90
+
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
 
 # Siepiński sieve
 title = "Siepiński sieve"
 axiom = "FXF--FF--FF"
 rules = {"F":"FF", "X":"--FXF++FXF++FXF--"}
-iterations = 7
+iterations = 2
 angle = 60
 width = 450
 aspect_ratio = 8 / 9
 margin = 35
 length = (width - 2 * margin) / 2 ** (iterations + 1 )
 angle_offset = -90
+
+# main(iterations, axiom, rules, angle, color=True, width=width) # ,  filename=title.replace(" ","-"))
